d850113a3cbf1450b3fdabf449f9ffd5233e9966
7u4n1011 <juanherdie@gmail.com>
2024-08-19 09:43:54 -0300

    Project Start

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask  (__name__)
+
+
+#codigo a completar
+
+nombre = "nombreDelProyecto"
+
+@app.route('/')
+
+def index():
+    return render_template('index.html', nombre=nombre)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
