65667118803904832fb04c6ed894d11375e8fc50
Agusmn7 <amenochio@escuelasproa.edu.ar>
2024-10-28 10:11:39 -0300

    Comenzamos a laburar (hay errores)

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from flask import *
 
+import mysql.connector
+
+# Conexion a la base de datos
+conexion = mysql.connector.connect(
+ host="localhost",
+ user="root",
+ password="root",
+ database="iphone"
+)
+cursor = conexion.cursor()
+
 app = Flask(__name__)
 
-#página principal
+# Página principal
 @app.route('/')
 def index():
   return render_template('index.html')
 
 
-#menu pedidos
-@app.route('/pedidos')
+# Menu pedidos
+@app.route('/pedido')
 def pedidos():
   pass
 
-#menu clientes
-@app.route('/clientes')
+# Menu clientes
+@app.route('/cliente')
 def clientes():
   pass
 
-#menu productos
+# Menu productos
 @app.route('/productos')
 def productos():
   pass
@@ -26,18 +37,53 @@ def productos():
 if __name__ == '__main__':
   app.run(debug=True)
 
+#-------------------------- Menu Clientes------------------------
 
-from flask import *
+@app.route('/cliente')
+def productos():
+  query = "SELECT * FROM cliente"
+  cursor.execute(query)
+  cliente = cursor.fetchall()
+  return render_template('productos.html',cliente=cliente)
 
-import mysql.connector
+@app.route('agregar_clientes', methods=['POST'])
+def agregar_clientes():
+  #obtengo los datos del formulario
+  nombre = request.form.get('nombre')
+  direccion = request.form.get('direccion')
+  dni = request.form.get('dni')
 
-conexion = mysql.connector.connect(
- host="localhost",
- user="root",
- password="root",
- database=""
-)
-cursor = conexion.cursor()
+  #los agrego a la base de datos
+  query = 'INSERT INTO cliente (descripcion, precio) VALUES (%s, %s, %s)'
+  cursor.execute(query, (nombre, direccion, dni))
+  conexion.commit()
+  return redirect(url_for('clientes'))
 
-app = Flask(__name__)
- 
+@app.route('/modificar_clientes', methods=['POST'])
+def modificar_clientes():
+  #obtengo el id que puso en el formulario
+  ID_cliente = request.form.get('ID')
+
+  #obtengo los campos modificados
+  nombre = request.form.get('nombre')
+  direccion = request.form.get('direccion')
+  dni = request.form.get('dni')
+
+  #ejecuto el sql para modificar
+  query = 'UPDATE cliente SET nombre = %s, direccion = %s, dni = %s WHERE ID_cliente = %s'
+  cursor.execute(query, (nombre, direccion, dni, ID_cliente))
+  return redirect(url_for('clientes'))
+
+@app.route('/eliminar_clientes', methods=['POST'])
+def eliminar_clientes():
+  #obtengo el id que puso en el formulario
+  ID_cliente = request.form.get('ID')
+
+  #hago la query en la base de datos para eliminar el producto de ese ID
+  query = 'DELETE FROM cliente WHERE '+ID_cliente+' = cliente.ID_cliente'
+  cursor.execute(query)
+  conexion.commit()
+  return redirect(url_for('clientes'))
+
+if __name__ == '__main__':
+  app.run(debug=True)
